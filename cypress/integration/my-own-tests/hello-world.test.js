06b8e2904c885cd1ed41cb55b8d3d35455091678
tejanhu <tejanahmedhu@gmail.com>
2022-03-11 16:45:43 +0000

    added custom data-test-id attribute

--- cypress/integration/my-own-tests/hello-world.test.js
+++ cypress/integration/my-own-tests/hello-world.test.js
@@ -5,5 +5,18 @@ describe('Blank test', () => {
         cy.visit("https://linkedin.com");
         // mocha syntax
         cy.contains("Welcome").should("exist");
+        cy.get("div#artdeco-global-alerts-cls-offset").should("exist");
+        // cy.get("div[id=artdeco-global-alerts-cls-offset]").should("exist");
+
+        cy.get("div#noartdeco-global-alerts-cls-offset").should("not.exist");
+
+        // Way 1 -> Brittle Method due to text possibly changing in future
+        // cy.contains("Suggested Searches");
+
+        // Way 2 -> Brittle Method due to class name i.e.: dummy class names (production) or element possibly changing in future
+        // cy.get('.etta-caps-header > h2');
+
+        // Way 3 -> Better way to use custom attribute
+        cy.get('[data-test-id="pill-list-module__cta"]');
     })
 })
