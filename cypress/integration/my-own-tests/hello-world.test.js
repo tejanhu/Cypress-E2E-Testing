8ea21f07b2250a64140242d8d9d77b544f35c21f
tejanhu <tejanahmedhu@gmail.com>
2022-03-15 12:59:52 +0000

    added another viewport

--- cypress/integration/my-own-tests/hello-world.test.js
+++ cypress/integration/my-own-tests/hello-world.test.js
@@ -25,6 +25,10 @@ describe('Basic Tests', () => {
     it('Every basic element exists', () => {
         cy.viewport(1280, 720)
         cy.visit("https://linkedin.com");
+    })
 
+    it('Every basic element exists on mobile', () => {
+        cy.viewport('iphone-x')
+        cy.visit("https://linkedin.com");
     })
 })
